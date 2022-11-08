c58d3de8083795f7377b5ce96af487c090c1d414
Aliana K <alianafatima2013@gmail.com>
2022-11-08 11:35:13 +0530

    Updated nav, hero and added about me section

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger')
     const nav = document.querySelector('.nav-links')
     const navLinks = document.querySelectorAll('.nav-links li')
+
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         navLinks.forEach((link, index)=>{
@@ -12,7 +13,32 @@ const navSlide = () => {
     });
     burger.classList.toggle('toggle');
     });
-    
-    
 }
+
+const navLinkHover = () => {
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index)=>{
+        if (link.style.animation)
+        link.style.animation = ''
+        else
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 +0.55}s`;
+    });
+    burger.classList.toggle('toggle');
+    });
+}
+
+const gradientEffect = () => {
+    const name = document.querySelector('.name')
+
+    name.addEventListener('hover', () => {
+
+        name.classList.toggle('gradient-effect');
+        
+    });
+}
+
 navSlide();
+gradientEffect();
