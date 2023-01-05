9365eeec920717f0323c30dfe4463c63c7d6222c
Aliana K <alianafatima2013@gmail.com>
2023-01-05 16:15:19 +0530

    Version 1.0 completed

--- app.js
+++ app.js
@@ -33,21 +33,6 @@ const navSlide = () => {
     });
 }
 
-const navLinkHover = () => {
-    const navLinks = document.querySelectorAll('.nav-links li')
-
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index)=>{
-        if (link.style.animation)
-        link.style.animation = ''
-        else
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 +0.55}s`;
-    });
-    burger.classList.toggle('toggle');
-    });
-}
-
 const gradientEffect = () => {
     const name = document.querySelector('.name')
 
@@ -68,16 +53,6 @@ colorThemes.forEach((themeOption) => {
 });
 
 document.onload = setTheme();
-
-var angle = 0;
-function galleryspin(sign) { 
-spinner = document.querySelector("#spinner");
-if (!sign) { angle = angle + 90; } else { angle = angle - 90; }
-spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-}
-
-// projectImage = document.querySelectorAll(".image-card")
-// function project
 
 navSlide();
 gradientEffect();
